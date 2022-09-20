7691d3b111d37320d1eef1cfe3f5afe44f790e2b
Rattlehead90 <strogonov@seznam.cz>
2022-09-20 17:41:53 +0200

    Add Square Class

---
+++ lib/board.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: false
+
+# stores  squares in a board_hash
+class Board
+
+end
---
+++ lib/square.rb
@@ -0,0 +1,16 @@
+# frozen_string_literal: false
+
+# simulates a square as a vertex in a board graph
+class Square
+  attr_accessor :name, :piece
+
+  def initialize(name, piece = nil)
+    @name = name
+    @piece = piece
+    @neighbor = []
+  end
+
+  def add_neighbor(square)
+    @neighbor << square
+  end
+end
