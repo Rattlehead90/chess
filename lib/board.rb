5b925f1d434955dc9836d3e22b816f7ee037a4fd
Rattlehead90 <strogonov@seznam.cz>
2022-09-24 18:29:19 +0200

    Move the pieces initialization to a separate module

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: false
 
+require 'colorize'
 require_relative '../lib/square'
 require_relative '../lib/piece'
 require_relative '../lib/pieces/rook'
@@ -8,8 +9,11 @@ require_relative '../lib/pieces/bishop'
 require_relative '../lib/pieces/king'
 require_relative '../lib/pieces/queen'
 require_relative '../lib/pieces/pawn'
+require_relative '../lib/starting_position'
+
 # stores squares in a board_hash, performs meta-functions of a graph
 class Board
+  include StartingPosition
   attr_accessor :board_hash
 
   def initialize
@@ -29,41 +33,7 @@ class Board
     end
   end
 
-  def place_pieces # consider rewriting with case
-    # rooks
-    @board_hash['a1'].piece,
-    @board_hash['h1'].piece = Rook.new('white')
-    @board_hash['a8'].piece,
-    @board_hash['h8'].piece = Rook.new('black')
-
-    # knights 
-    @board_hash['b1'].piece,
-    @board_hash['g1'].piece = Knight.new('white')
-    @board_hash['b1'].piece,
-    @board_hash['b8'].piece = Knight.new('black')
-
-    # bishops
-    @board_hash['c1'].piece,
-    @board_hash['f1'].piece = Bishop.new('white')
-    @board_hash['c8'].piece,
-    @board_hash['f8'].piece = Bishop.new('black')
-
-    # kings
-    @board_hash['e1'].piece = King.new('white')
-    @board_hash['e8'].piece = King.new('black')
-
-    # queens 
-    @board_hash['d1'].piece = Queen.new('white')
-    @board_hash['d8'].piece = Queen.new('black')
-
-    # pawns
-    second_rank = @board_hash.keys.select { |square| square.include?('2') }
-    seventh_rank = @board_hash.keys.select { |square| square.include?('7') }
-    second_rank.each { |square| @board_hash[square].piece = Pawn.new('white') }
-    seventh_rank.each { |square| @board_hash[square].piece = Pawn.new('black') }
-  end
-
-  # def display
+  def display
     
-  # end
+  end
 end
---
+++ lib/starting_position.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: false
+
+# part of the game initialization. Placing the pieces on starting squares.
+module StartingPosition
+  def place_rooks
+    @board_hash['a1'].piece,
+    @board_hash['h1'].piece = Rook.new('white')
+    @board_hash['a8'].piece,
+    @board_hash['h8'].piece = Rook.new('black')
+  end
+
+  def place_knights
+    @board_hash['b1'].piece,
+    @board_hash['g1'].piece = Knight.new('white')
+    @board_hash['b1'].piece,
+    @board_hash['b8'].piece = Knight.new('black')
+  end
+
+  def place_bishops
+    @board_hash['c1'].piece,
+    @board_hash['f1'].piece = Bishop.new('white')
+    @board_hash['c8'].piece,
+    @board_hash['f8'].piece = Bishop.new('black')
+  end
+
+  def place_kings
+    @board_hash['e1'].piece = King.new('white')
+    @board_hash['e8'].piece = King.new('black')
+  end
+
+  def place_queens
+    @board_hash['d1'].piece = Queen.new('white')
+    @board_hash['d8'].piece = Queen.new('black')
+  end
+
+  def place_pawns
+    second_rank = @board_hash.keys.select { |square| square.include?('2') }
+    seventh_rank = @board_hash.keys.select { |square| square.include?('7') }
+    second_rank.each { |square| @board_hash[square].piece = Pawn.new('white') }
+    seventh_rank.each { |square| @board_hash[square].piece = Pawn.new('black') }
+  end
+
+  def place_pieces
+    place_rooks
+    place_knights
+    place_bishops
+    place_kings
+    place_queens
+    place_pawns
+  end
+end
